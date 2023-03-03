30e2d888ab1c3d56b69417e5325a408f4d68e23c
Ryan Lai <ryanlai12@gmail.com>
2023-03-03 21:02:59 +0000

    Implemented password encryption using bcryptjs

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 const User = require('./models/User')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 require('dotenv').config()
 const mongoURI = process.env.MONGO_CRITTER_URI;
@@ -12,10 +13,15 @@ app.use(express.json())
 
 mongoose.connect(mongoURI);
 
+const salt = bcrypt.genSaltSync(4);
+
 app.post('/register', async (req,res) => {
   const {username, password} = req.body;
   try {
-    const userDoc = await User.create({username, password})
+    const userDoc = await User.create({
+      username, 
+      password: bcrypt.hashSync(password, salt)
+    })
     res.json(userDoc)
   } catch(e) {
     res.status(400).json(e.message)
