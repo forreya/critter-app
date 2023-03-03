3ac2afa4d8039c246bc40f6cbd73cf62876f3d6e
Ryan Lai <ryanlai12@gmail.com>
2023-03-03 12:41:09 +0000

    Added initial index routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Post from './Post'
 import Header from './Header'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <main className="main-container">
-      <Header />
-      <div className="chirp-container">
-        <Post />
-        <Post />
-      </div>
-    </main>
+    <Routes>
+      <Route index element={
+        <main className="main-container">
+          <Header />
+          <div className="chirp-container">
+            <Post />
+            <Post />
+          </div>
+        </main>
+      }></Route>
+    </Routes>
   );
 }
 
