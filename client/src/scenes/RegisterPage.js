b786254eb87f9da5651a694c5f1a890063f0e33f
Ryan Lai <ryanlai12@gmail.com>
2023-03-03 16:40:58 +0000

    Implemented POST method for registration page, allowing user to submit JSON string containing username and password

--- client/src/scenes/RegisterPage.js
+++ client/src/scenes/RegisterPage.js
@@ -1,12 +1,31 @@
 import '../styles/RegisterPage.css'
+import {useState} from 'react'
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type': 'application/json'}
+    })
+  }
+
   return (
     <div className="register-container">
       <h1 className="register-title">Register</h1>
-      <form className="register-form">
-        <input type='text' placeholder="username"></input>
-        <input type='text' placeholder="password"></input>
+      <form className="register-form" onSubmit={handleRegister}>
+        <input type='text' 
+               placeholder="username"
+               value = {username}
+               onChange = {event => setUsername(event.target.value)} />
+        <input type='text' 
+               placeholder="password"
+               value = {password}
+               onChange = {event => setPassword(event.target.value)} />
         <button>Register</button>
       </form>
     </div>
