8d88285b3ddf422cd07e9ffa536ab08115f6c443
Ryan Lai <ryanlai12@gmail.com>
2023-03-05 14:33:43 +0000

    Finished prelimiary login system (using JWT & cookies)

--- client/src/scenes/LoginPage.js
+++ client/src/scenes/LoginPage.js
@@ -1,12 +1,44 @@
 import '../styles/LoginPage.css'
+import {useState} from 'react'
+import { Navigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirectToHome, setRedirectToHome] = useState(false);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+    })
+    if (response.ok) {
+      setRedirectToHome(true);
+    } else {
+      alert("Incorrect Credentials.")
+    }
+  }
+
+  if (redirectToHome) {
+    return <Navigate to={'/'}/>
+  }
+
+
   return (
     <div className="login-container">
-    <h1 className="login-title">Login</h1>
-      <form className="login-form">
-        <input type='text' placeholder="username"></input>
-        <input type='text' placeholder="password"></input>
+      <h1 className="login-title">Login</h1>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input type='text' 
+               placeholder="username" 
+               value={username}
+               onChange={event => setUsername(event.target.value)}/>
+        <input type='text' 
+               placeholder="password" 
+               value={password} 
+               onChange={event => setPassword(event.target.value)}/>
         <button>Login</button>
       </form>
     </div>
--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ const app = express()
 const User = require('./models/User')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config()
 const mongoURI = process.env.MONGO_CRITTER_URI;
+const secretKey = process.env.SECRET_KEY;
 
-app.use(cors())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json())
 
 mongoose.connect(mongoURI);
@@ -25,6 +27,20 @@ app.post('/register', async (req,res) => {
     res.json(userDoc)
   } catch(e) {
     res.status(400).json(e.message)
+  }
+})
+
+app.post('/login', async (req,res) => {
+  const {username, password} = req.body;
+  const userDoc = await User.findOne({username});
+  const doesPassMatch = bcrypt.compareSync(password, userDoc.password)
+  if (doesPassMatch) {
+    jwt.sign({username, id:userDoc._id}, secretKey, {}, (err,token) => {
+      if (err) throw err;
+      res.cookie('token', token).json('Token created.')
+    })
+  } else {
+    res.status(400).json('Incorrect Credentials.')
   }
 })
 
